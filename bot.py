b441233ac6addc0a45683651edb17f6eecc62bb2
Alex <admin@docode.ru>
2022-11-25 20:33:27 +0300

    project structure

---
+++ bot.py
@@ -0,0 +1,38 @@
+import logging
+
+from telegram.ext import Updater, CommandHandler
+
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+TOKEN = "516449158:AAHfQLPIjrxlKPe5f5kFqsFuTDGnS5hPNv8"
+
+
+def main() -> None:
+    """Start the bot."""
+
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(TOKEN)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    # dispatcher.add_handler(CommandHandler("start", start))
+
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
